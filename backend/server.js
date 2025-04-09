ab60aec47879b3903a3326bc9d4d2336343c6c63
deshpanda <samyakd.2001@gmail.com>
2025-04-10 00:04:48 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 
 // Routes
 app.use('/api/jobs', jobRoutes);
